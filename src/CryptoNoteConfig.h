bfaf8fe998f225c77cefbec859a655cd1dd12fa2
brokingtools <35738232+brokingtools@users.noreply.github.com>
2018-01-29 16:25:39 +0530

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -144,7 +144,7 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
 
 const std::initializer_list<const char*> SEED_NODES = {
-   "194.135.91.245:17236",
+   "45.32.54.89:17236",
    "80.208.228.138:17236"
 };
 
